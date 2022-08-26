5aaa389461c9a3e86a039a5c61c6548828036d4c
UcGeorge <petegeorge20005@gmail.com>
2022-08-26 22:25:21 +0100

    Added models

---
+++ lib/data/models/book.dart
@@ -0,0 +1,116 @@
+import 'dart:convert';
+
+import 'chapter.dart';
+import 'shen_image.dart';
+
+class Book {
+  Book({
+    required this.id,
+    required this.name,
+    required this.type,
+    required this.link,
+    required this.source,
+    this.status,
+    this.rating,
+    this.description,
+    this.chapterCount,
+    this.coverPicture,
+    this.chapters,
+  });
+
+  factory Book.fromJson(String source) => Book.fromMap(json.decode(source));
+
+  factory Book.fromMap(Map<String, dynamic> map) {
+    return Book(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      type: bookTypeFromMap(map['type']),
+      link: map['link'] ?? '',
+      source: map['source'] ?? '',
+      status: map['status'] != null ? bookStatusFromMap(map['status']) : null,
+      rating: map['rating'],
+      description: map['description'],
+      chapterCount: map['chapterCount']?.toInt(),
+      coverPicture: map['coverPicture'] != null
+          ? ShenImage.fromMap(map['coverPicture'])
+          : null,
+      chapters: map['chapters'] != null
+          ? List<Chapter>.from(map['chapters']?.map((x) => Chapter.fromMap(x)))
+          : null,
+    );
+  }
+
+  final int? chapterCount;
+  final List<Chapter>? chapters;
+  final ShenImage? coverPicture;
+  final String? description;
+  final String id;
+  final String link;
+  final String name;
+  final String? rating;
+  final String source;
+  final BookStatus? status;
+  final BookType type;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'type': type.toMap(),
+      'link': link,
+      'source': source,
+      'status': status?.toMap(),
+      'rating': rating,
+      'description': description,
+      'chapterCount': chapterCount,
+      'coverPicture': coverPicture?.toMap(),
+      'chapters': chapters?.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  static bookTypeFromMap(String value) {
+    switch (value) {
+      case 'novel':
+        return BookType.novel;
+      case 'manga':
+        return BookType.manga;
+    }
+  }
+
+  static bookStatusFromMap(String value) {
+    switch (value) {
+      case 'ongoing':
+        return BookStatus.ongoing;
+      case 'completed':
+        return BookStatus.completed;
+    }
+  }
+}
+
+enum BookType { novel, manga }
+
+enum BookStatus { ongoing, completed }
+
+extension EnumSerialisation on BookType {
+  String toMap() {
+    switch (this) {
+      case BookType.manga:
+        return 'manga';
+      case BookType.novel:
+        return 'novel';
+    }
+  }
+}
+
+extension BookStatusSerialisation on BookStatus {
+  String toMap() {
+    switch (this) {
+      case BookStatus.ongoing:
+        return 'ongoing';
+      case BookStatus.completed:
+        return 'completed';
+    }
+  }
+}
---
+++ lib/data/models/chapter.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'shen_image.dart';
+
+class Chapter {
+  Chapter({
+    required this.id,
+    required this.name,
+    required this.link,
+    required this.source,
+    this.chapterImages,
+    this.chapterParagraphs,
+  });
+
+  factory Chapter.fromJson(String source) =>
+      Chapter.fromMap(json.decode(source));
+
+  factory Chapter.fromMap(Map<String, dynamic> map) {
+    return Chapter(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      link: map['link'] ?? '',
+      source: chapterSourceFromMap(map['source']),
+      chapterImages: map['chapterImages'] != null
+          ? List<ShenImage>.from(
+              map['chapterImages']?.map((x) => ShenImage.fromMap(x)))
+          : null,
+      chapterParagraphs: List<String>.from(map['chapterParagraphs']),
+    );
+  }
+
+  final List<ShenImage>? chapterImages;
+  final List<String>? chapterParagraphs;
+  final String id;
+  final String link;
+  final String name;
+  final ChapterSource source;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'link': link,
+      'source': source.toMap(),
+      'chapterImages': chapterImages?.map((x) => x.toMap()).toList(),
+      'chapterParagraphs': chapterParagraphs,
+    };
+  }
+
+  static ChapterSource chapterSourceFromMap(String source) =>
+      source == 'network' ? ChapterSource.network : ChapterSource.file;
+
+  String toJson() => json.encode(toMap());
+}
+
+enum ChapterSource { network, file }
+
+extension EnumSerialisation on ChapterSource {
+  String toMap() => this == ChapterSource.network ? 'network' : 'file';
+}
