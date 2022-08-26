040c7a26790dea64b20d3fb3f18ec7296238cf26
UcGeorge <petegeorge20005@gmail.com>
2022-08-27 00:29:35 +0100

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,6 +3,6 @@ import 'package:shenku/data/sources/source.dart';
 import 'package:shenku/data/sources/wuxia_world.dart';
 
 const appVer = '1.0.0';
-String appDir(String appDocDir) => '$appDocDir\\PGL';
+String appDir(String appDocDir) => '$appDocDir\\Shen-Ku';
 String dataFileDir(String appDocDir) => '${appDir(appDocDir)}\\app-data.json';
 List<BookSource> appBookSources = [Manganelo(), WuxiaWorld()];
